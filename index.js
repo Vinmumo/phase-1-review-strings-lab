03f25c02e05d9a94a6fef6d5e24de95e7a008830
vincent <vinnymummo@gmail.com>
2024-03-18 11:43:38 +0300

    my commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const currentUser = 'Vincent Mumo';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 // Write your code in this file!
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
+// const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
